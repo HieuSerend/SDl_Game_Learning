0bbb6fec0b814eedb45125ff00018aad8e551980
Hieeus <lehieutz2004@gmail.com>
2024-03-29 13:38:05 +0700

    17_mouse_events

--- main/main.cpp
+++ main/main.cpp
@@ -1,276 +1,463 @@
-
 #include <SDL.h>
 #include <SDL_image.h>
-#include <SDL_ttf.h>
 #include <bits/stdc++.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-class LTexture{
-    public:
+const int BUTTON_WIDTH = 300;
+const int BUTTON_HEIGHT = 200;
+const int TOTAL_BUTTONS = 4;
 
-        LTexture();
-        ~LTexture();
+enum LButtonSprite
+{
+	BUTTON_SPRITE_MOUSE_OUT = 0,
+	BUTTON_SPRITE_MOUSE_OVER_MOTION = 1,
+	BUTTON_SPRITE_MOUSE_DOWN = 2,
+	BUTTON_SPRITE_MOUSE_UP = 3,
+	BUTTON_SPRITE_TOTAL = 4
+};
 
-        bool LoadFromFile(std::string path);
+//Texture wrapper class
+class LTexture
+{
+	public:
 
-        void free();
+		LTexture();
+		~LTexture();
 
-        bool TextLoading(std::string text, SDL_Color color);
+		bool loadFromFile( std::string path );
 
-        void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE );
+		#if defined(SDL_TTF_MAJOR_VERSION)
+		//Creates image from font string
+		bool loadFromRenderedText( std::string textureText, SDL_Color textColor );
+		#endif
 
-        int getWidth();
-        int getHeight();
+		void free();
 
-    private:
-        SDL_Texture* mTexture;
-        int mWidth;
-        int mHeight;
+		//Set color modulation
+		void setColor( Uint8 red, Uint8 green, Uint8 blue );
+
+		//Set blending
+		void setBlendMode( SDL_BlendMode blending );
+
+		//Set alpha modulation
+		void setAlpha( Uint8 alpha );
+
+		void render( int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE );
+
+		int getWidth();
+		int getHeight();
+
+	private:
+		SDL_Texture* mTexture;
+
+		int mWidth;
+		int mHeight;
+};
+
+//The mouse button
+class LButton
+{
+	public:
+		LButton();
+
+		void setPosition( int x, int y );
+
+		void handleEvent( SDL_Event* e );
+
+		void render();
+
+	private:
+		SDL_Point mPosition;
+
+		LButtonSprite mCurrentSprite;
 };
 
 bool init();
-bool media();
+
+bool loadMedia();
+
 void close();
 
-SDL_Window* Window = NULL;
-SDL_Renderer* Renderer = NULL;
+SDL_Window* gWindow = NULL;
 
-TTF_Font* gFont;
+SDL_Renderer* gRenderer = NULL;
 
-LTexture gFontTexture;
+SDL_Rect gSpriteClips[ BUTTON_SPRITE_TOTAL ];
+LTexture gButtonSpriteSheetTexture;
 
-LTexture::LTexture(){
+LButton gButtons[ TOTAL_BUTTONS ];
 
-    mTexture = NULL;
-    mWidth = 0;
-    mHeight = 0;
-
+LTexture::LTexture()
+{
+	//Initialize
+	mTexture = NULL;
+	mWidth = 0;
+	mHeight = 0;
 }
 
-LTexture::~LTexture(){
-
-    free();
-
+LTexture::~LTexture()
+{
+	//Deallocate
+	free();
 }
 
-void LTexture::free(){
+bool LTexture::loadFromFile( std::string path )
+{
+	//Get rid of preexisting texture
+	free();
 
-    if (mTexture != NULL){
+	//The final texture
+	SDL_Texture* newTexture = NULL;
 
-        SDL_DestroyTexture(mTexture);
-        mWidth = 0;
-        mHeight = 0;
+	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+	if( loadedSurface == NULL )
+	{
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+	}
+	else
+	{
+		//Color key image
+		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
 
-    }
+		//Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+		if( newTexture == NULL )
+		{
+			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = loadedSurface->w;
+			mHeight = loadedSurface->h;
+		}
 
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	//Return success
+	mTexture = newTexture;
+	return mTexture != NULL;
 }
 
-bool LTexture::TextLoading(std::string text, SDL_Color color){
+#if defined(SDL_TTF_MAJOR_VERSION)
+bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor )
+{
+	//Get rid of preexisting texture
+	free();
 
-    LTexture::free();
+	//Render text surface
+	SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor );
+	if( textSurface == NULL )
+	{
+		printf( "Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError() );
+	}
+	else
+	{
+		//Create texture from surface pixels
+        mTexture = SDL_CreateTextureFromSurface( gRenderer, textSurface );
+		if( mTexture == NULL )
+		{
+			printf( "Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = textSurface->w;
+			mHeight = textSurface->h;
+		}
 
-    SDL_Surface* FontSurface = TTF_RenderText_Solid(gFont, text.c_str(), color);
+		//Get rid of old surface
+		SDL_FreeSurface( textSurface );
+	}
 
-    if (FontSurface == NULL){
-        std::cout << "Could not render font surface! TTF error" << std::endl;
-        TTF_GetError();
-    }
-    else{
-        mTexture = SDL_CreateTextureFromSurface(Renderer, FontSurface);
-        if (mTexture == NULL){
-            std::cout << "Could not create texture from surface! SDL error: " << std::endl;
-            TTF_GetError();
-        }
-        else{
-            mWidth = FontSurface->w;
-            mHeight = FontSurface->h;
-        }
-    }
+	//Return success
+	return mTexture != NULL;
+}
+#endif
 
-    SDL_FreeSurface(FontSurface);
-    return mTexture != NULL;
-
+void LTexture::free()
+{
+	if( mTexture != NULL )
+	{
+		SDL_DestroyTexture( mTexture );
+		mTexture = NULL;
+		mWidth = 0;
+		mHeight = 0;
+	}
 }
 
-void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip){
-
-    SDL_Rect renderQuad = {x, y, mWidth, mHeight};
-
-    if (clip != NULL){
-        renderQuad.w = clip->w;
-        renderQuad.h = clip->h;
-    }
-
-    SDL_RenderCopyEx(Renderer, mTexture, clip , &renderQuad, angle, center, flip);
-
+void LTexture::setColor( Uint8 red, Uint8 green, Uint8 blue )
+{
+	SDL_SetTextureColorMod( mTexture, red, green, blue );
 }
 
- LTexture::getWidth(){
-    return mWidth;
+void LTexture::setBlendMode( SDL_BlendMode blending )
+{
+	SDL_SetTextureBlendMode( mTexture, blending );
 }
 
- LTexture::getHeight(){
-    return mHeight;
+void LTexture::setAlpha( Uint8 alpha )
+{
+	SDL_SetTextureAlphaMod( mTexture, alpha );
 }
 
-bool init(){
+void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
+{
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
 
-    bool success = true;
+	//Set clip rendering dimensions
+	if( clip != NULL )
+	{
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0){
-        std::cout << "Could not initialize SDL! SDL error: " << std::endl;
-        SDL_GetError();
-        success = false;
-    }
-    else{
-        if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+	//Render to screen
+	SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip );
+}
+
+int LTexture::getWidth()
+{
+	return mWidth;
+}
+
+int LTexture::getHeight()
+{
+	return mHeight;
+}
+
+LButton::LButton()
+{
+	mPosition.x = 0;
+	mPosition.y = 0;
+
+	mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
+}
+
+void LButton::setPosition( int x, int y )
+{
+	mPosition.x = x;
+	mPosition.y = y;
+}
+
+void LButton::handleEvent( SDL_Event* e )
+{
+	//If mouse event happened
+	if( e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP )
+	{
+		int x, y;
+		SDL_GetMouseState( &x, &y );
+
+		bool inside = true;
+
+		if( x < mPosition.x )
+		{
+			inside = false;
+		}
+
+		else if( x > mPosition.x + BUTTON_WIDTH )
+		{
+			inside = false;
+		}
+
+		else if( y < mPosition.y )
+		{
+			inside = false;
+		}
+
+		else if( y > mPosition.y + BUTTON_HEIGHT )
+		{
+			inside = false;
+		}
+
+		if( !inside )
+		{
+			mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
+		}
+
+		else
+		{
+			switch( e->type )
+			{
+				case SDL_MOUSEMOTION:
+				mCurrentSprite = BUTTON_SPRITE_MOUSE_OVER_MOTION;
+				break;
+
+				case SDL_MOUSEBUTTONDOWN:
+				mCurrentSprite = BUTTON_SPRITE_MOUSE_DOWN;
+				break;
+
+				case SDL_MOUSEBUTTONUP:
+				mCurrentSprite = BUTTON_SPRITE_MOUSE_UP;
+				break;
+			}
+		}
+	}
+}
+
+void LButton::render()
+{
+	gButtonSpriteSheetTexture.render( mPosition.x, mPosition.y, &gSpriteClips[ mCurrentSprite ] );
+}
+
+bool init()
+{
+	bool success = true;
+
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
 		{
 			printf( "Warning: Linear texture filtering not enabled!" );
 		}
-        Window = SDL_CreateWindow("Adventure time", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (Window == NULL){
-            std::cout << "Could not create window! SDL error: " << std::endl;
-            SDL_GetError();
-            success = false;
-        }
-        else{
-            Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-            if (Renderer == NULL){
-                std::cout << "Could not create renderer! SDL error: " << std::endl;
-                SDL_GetError();
-                success = false;
-            }
-            else{
-                SDL_SetRenderDrawColor(Renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-                int imgFlags = IMG_INIT_PNG;
-                if (!(IMG_Init(imgFlags) & imgFlags)){
-                    std::cout << "Could not initialize image flags! IMG error: " <<  std::endl;
-                    IMG_GetError();
-                    success = false;
-                }
 
-                if (TTF_Init() == -1){
-                    std::cout << "Could not initialize true type font! TTF error: " << std::endl;
-                    TTF_GetError();
-                    success = false;
-                }
-            }
+		//Create window
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			//Create vsynced renderer for window
+			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+			if( gRenderer == NULL )
+			{
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 
-        }
-    }
-    return success;
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
 }
 
-bool LTexture::LoadFromFile(std::string path){
+bool loadMedia()
+{
+	bool success = true;
 
-    SDL_Texture* newTexture = NULL;
+	//Load sprites
+	if( !gButtonSpriteSheetTexture.loadFromFile( "17_mouse_events/button.png" ) )
+	{
+		printf( "Failed to load button sprite texture!\n" );
+		success = false;
+	}
+	else
+	{
+		//Set sprites
+		for( int i = 0; i < BUTTON_SPRITE_TOTAL; ++i )
+		{
+			gSpriteClips[ i ].x = 0;
+			gSpriteClips[ i ].y = i * 200;
+			gSpriteClips[ i ].w = BUTTON_WIDTH;
+			gSpriteClips[ i ].h = BUTTON_HEIGHT;
+		}
 
-    SDL_Surface* Image_Surface = IMG_Load(path.c_str());
+		//Set buttons in corners
+		gButtons[ 0 ].setPosition( 0, 0 );
+		gButtons[ 1 ].setPosition( SCREEN_WIDTH - BUTTON_WIDTH, 0 );
+		gButtons[ 2 ].setPosition( 0, SCREEN_HEIGHT - BUTTON_HEIGHT );
+		gButtons[ 3 ].setPosition( SCREEN_WIDTH - BUTTON_WIDTH, SCREEN_HEIGHT - BUTTON_HEIGHT );
+	}
 
-    if (Image_Surface == NULL){
-        std::cout << "Could not load image surface! IMG error: " ;
-        IMG_GetError();
-    }
-    else{
-        SDL_SetColorKey(Image_Surface, SDL_TRUE, SDL_MapRGB(Image_Surface->format, 0, 255, 255));
-
-        newTexture = SDL_CreateTextureFromSurface(Renderer, Image_Surface);
-        if (newTexture == NULL){
-            std::cout << "Unable to create texture from surface! SDL2 error: ";
-            SDL_GetError();
-        }
-        else{
-            mWidth = Image_Surface->w;
-            mHeight = Image_Surface->h;
-        }
-    }
-
-    SDL_FreeSurface(Image_Surface);
-    mTexture = newTexture;
-
-    return mTexture != NULL;
+	return success;
 }
 
-bool media(){
+void close()
+{
+	//Free loaded images
+	gButtonSpriteSheetTexture.free();
 
-    bool success = true;
+	//Destroy window
+	SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	gRenderer = NULL;
 
-    gFont = TTF_OpenFont("main/resources/lazy.ttf", 28);
-
-    if (gFont == NULL){
-        std::cout << "Could not open font! ttf error: " << std::endl;
-        TTF_GetError();
-        success = false;
-    }
-    else{
-        SDL_Color color = {125,125,125};
-        if (!(gFontTexture.TextLoading("test a short text", color))){
-            std::cout << "Could not load text! TTF error: " << std::endl;
-            TTF_GetError();
-            success = false;
-        }
-    }
-
-    return success;
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
 }
 
-void close(){
+int main( int argc, char* args[] )
+{
+	//Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		//Load media
+		if( !loadMedia() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{
+			bool quit = false;
 
-    gFontTexture.free();
+			SDL_Event e;
 
-    TTF_CloseFont(gFont);
-    gFont = NULL;
+			//While application is running
+			while( !quit )
+			{
+				//Handle events on queue
+				while( SDL_PollEvent( &e ) != 0 )
+				{
+					if( e.type == SDL_QUIT )
+					{
+						quit = true;
+					}
 
-    SDL_DestroyRenderer(Renderer);
-    Renderer = NULL;
-    SDL_DestroyWindow(Window);
-    Window = NULL;
+					for( int i = 0; i < TOTAL_BUTTONS; ++i )
+					{
+						gButtons[ i ].handleEvent( &e );
+					}
+				}
 
-    TTF_Quit();
-    IMG_Quit();
-    SDL_Quit();
+				//Clear screen
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+				SDL_RenderClear( gRenderer );
 
-}
+				//Render buttons
+				for( int i = 0; i < TOTAL_BUTTONS; ++i )
+				{
+					gButtons[ i ].render();
+				}
 
-int main(int argc, char* args[]){
+				//Update screen
+				SDL_RenderPresent( gRenderer );
+			}
+		}
+	}
 
-    if (!init()){
-        std::cout << "Could not initialize sdl2! sdl error: " << std::endl;
-        SDL_GetError();
-    }
-    else{
-        if (!media()){
-            std::cout << "Could not load image surface! img error: " << std::endl;
-            IMG_GetError();
-        }
-        else{
-            bool quit = false;
+	//Free resources and close SDL
+	close();
 
-            SDL_Event e;
-
-            while (!quit){
-                while (SDL_PollEvent(&e) != 0){
-                    if (e.type == SDL_QUIT){
-                        quit = true;
-                    }
-
-                }
-
-                //Clear screen
-				SDL_SetRenderDrawColor( Renderer, 0xFF, 0xFF, 0xFF, 0xFF );
-				SDL_RenderClear( Renderer );
-
-				gFontTexture.render((SCREEN_WIDTH - gFontTexture.getWidth())/2, (SCREEN_HEIGHT - gFontTexture.getHeight())/2);
-
-				SDL_RenderPresent( Renderer );
-
-            }
-        }
-    }
-
-    void close();
-
-    return 0;
+	return 0;
 }
